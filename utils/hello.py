e5af6b20431cb8cd4cb3c7196189873577e36e33
Julien <juliencaron@protonmail.com>
2022-06-28 01:08:50 +0200

    add generate_sample_data script

--- utils/hello.py
+++ utils/hello.py
@@ -1,2 +1,15 @@
 def hello(name: str) -> str:
+    '''
+    Function that's greeting to test export function to notebooks
+
+    Parameters
+    ==========
+    name: str
+        Say hello to {name}
+
+    Returns
+    =======
+    str
+        The greeting sentence
+    '''
     return f'Hello, {name}!'
